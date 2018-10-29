0d58fb17602c6cbc4f55e18acdef7b12b338c524
Paul Zabelin <pzabelin+git@gmail.com>
2018-10-29 10:26:22 -0700

    Initial Commit

---
+++ EmailClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmailClient
+//
+//  Created by Paul Zabelin on 10/29/18.
+//  Copyright © 2018 Paul Zabelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
