e3dc0a3bf2db3a8ee1bf811b484dad9a726cc348
Paul Zabelin <pzabelin+git@gmail.com>
2018-11-01 16:26:28 -0700

    show unread count static text

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -11,14 +11,25 @@ import XCTest
 class UITests: XCTestCase {
     let app = XCUIApplication()
 
+    func launchOnce() {
+        if app.state != .runningForeground {
+            app.launch()
+        }
+    }
+
     override func setUp() {
         continueAfterFailure = false
-        app.launch()
+        launchOnce()
     }
 
     func testSections() {
         let unreadSection = app.staticTexts["UNREAD EMAILS"]
         XCTAssert(unreadSection.exists, "there should be a section for unread emails")
         XCTAssert(app.staticTexts["READ EMAILS"].exists, "there should be a section for unread emails")
+    }
+
+    func testUnreadCount() {
+        let unreadCount = app.buttons["Unread count: 2"]
+        XCTAssert(unreadCount.exists, "should show 2 unread emails")
     }
 }
