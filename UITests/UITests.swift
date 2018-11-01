b222911aee4a8aa6b2ce9cf12e390911376549e4
Paul Zabelin <pzabelin+git@gmail.com>
2018-11-01 13:45:34 -0700

    cleanup main target test bundles

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class EmailClientUITests: XCTestCase {
+class UITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
